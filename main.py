6cf5f474ba98103182ecc301ebdd029556cd157f
rishavahuja <rishavahuja73@gmail.com>
2023-01-06 16:38:10 +0530

    updated main.py and requirements.txt for using with render postgress db

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from forms import CreatePostForm
 from flask_gravatar import Gravatar
 from functools import wraps
 import forms
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY','8BYkEfBA6O6donzWlSihBXox7C0sKR6b')
 app.config['CKEDITOR_PKG_TYPE'] = 'basic'
 
 ckeditor = CKEditor(app)
@@ -29,7 +30,8 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
